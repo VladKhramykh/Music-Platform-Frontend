4e2fd5c953e3282d9f8ae86566a7e9ca9153af74
Vlad Khramykh <vlad.kramykh@wedextim.com>
2020-11-25 19:58:32 +0300

    initial commit with basic the structure and functionality of the application

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AlbumComponent} from './album/album.component';
+import {TracksComponent} from './tracks/tracks.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/music', pathMatch: 'full'},
+  {path: 'music', component: HomeComponent},
+  {path: ':id/:name', component: AlbumComponent},
+  {path: ':id/:name/:colllection_id/:collection_name', component: TracksComponent},
+  {path: '***', redirectTo: '/music', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule],
+  declarations: [],
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule, HttpClientJsonpModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatRippleModule} from '@angular/material/core';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {ArtistComponent} from './artist/artist.component';
+import {AlbumComponent} from './album/album.component';
+import {TrackComponent} from './track/track.component';
+import {PlayerComponent} from './player/player.component';
+import {TrackControlComponent} from './track/track-control/track-control.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from './home/home.component';
+import {TracksComponent} from './tracks/tracks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent,
+    AlbumComponent,
+    TrackComponent,
+    PlayerComponent,
+    TrackControlComponent,
+    HomeComponent,
+    TracksComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatListModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatRippleModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
